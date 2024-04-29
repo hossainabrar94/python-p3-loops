a83764033a817ba7686ce07a08b3542cd3ed0302
Abrar <hossainabrar@aol.com>
2024-04-29 19:14:05 -0400

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 0
+    while i<11:
+        print (i)
+        i += 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_integers = list()
+    for integer in int_list:
+        squared_integer = integer * integer
+        squared_integers.append(squared_integer)
+    return squared_integers
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i% 5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
+            print("Fizz")
+        elif i%5 == 0:
+            print('Buzz')
+        else:
+            print(i)
